0b281167509f722a4bd397b931558e3a9df96ac5
Refik Mecidli <66335801+warlockmdwa@users.noreply.github.com>
2020-09-24 13:07:44 +0400

    Update Convert minutes to seconds.py

--- Convert minutes to seconds.py
+++ Convert minutes to seconds.py
@@ -1,10 +1,11 @@
+
 while True:
 
-    fey = int(input('Write value for converting seconds: '))
+    v = int(input('Write value for converting seconds: '))
 
-    def converter(fey):
-        sec_value = fey % (3600 * 24)
+    def converter(v):
+        sec_value = v % (3600 * 24)
         min = sec_value * 60
         print("Converted minute value to seconds: ", min)
 
-    converter(fey)
+    converter(v)
